f5a896788abf74832853f100a80dbe02c3e5b27d
jewelpinto890 <72159443+jewelpinto890@users.noreply.github.com>
2022-10-28 22:21:54 +0530

    Add files via upload

---
+++ news/newsletter.php
@@ -0,0 +1,28 @@
+
+<?php 
+$user = "root";  
+$password = "";  
+$host = "localhost";  
+$dbase = "newslettersdb";  
+$table = "letters";  
+ 
+
+$pass= $_POST['pass']; 
+$email= $_POST['email']; 
+  
+// Connection to DBase  
+$dbc= mysqli_connect($host,$user,$password, $dbase)  
+or die("Unable to select database"); 
+ 
+ 
+$query= "INSERT INTO $table  ". "VALUES ('$email', '$pass')"; 
+ 
+mysqli_query ($dbc, $query) 
+or die ("Error querying database"); 
+ 
+echo 'You have been successfully added.' . '<br>'; 
+ 
+mysqli_close($dbc); 
+ 
+?> 
+ 
